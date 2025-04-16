59dc3558e9ece7d4192c49058268491f9b0a4d2d
S5ntx <s13382365058@163.com>
2025-04-16 15:21:59 +0800

    notes

--- v1/include/MemoryPool.h
+++ v1/include/MemoryPool.h
@@ -1,25 +1,58 @@
-#pragma once 
+#pragma once                //GCC、Clang、MSVC 
 
-#include <atomic>
-#include <cassert>
-#include <cstdint>
+// #pragma once 等同于
+
+// ifndef MEMORY_POOL_H      如果没有定义 MEMORY_POOL_H 宏，就执行下面的代码
+// #define MEMORY_POOL_H     表示定义了这个宏 MEMORY_POOL_H，告诉编译器“我已经包含过这个头文件了”
+
+// ...
+
+// endif                     和 #ifndef 配对，表示条件编译的结束 
+// 防止头文件被重复包含
+
+
+// 当你#include <>的时候
+// 编译器做的不是“记录你引用了哪个文件”，而是在预处理阶段直接把这个文件的内容完整复制过来
+
+// 多次声明（如 void f();）不会报错
+// 多次定义类/函数体  和 多次定义全局变量 会报错
+
+
+#include <atomic>           // 提供原子操作的支持。
+#include <cassert>          // 提供断言宏，用于调试时检查假设。
+#include <cstdint>          // 提供标准的整数类型，如int32_t、uint64_t等。
 #include <iostream>
-#include <memory>
-#include <mutex>
+#include <memory>           // 用于智能指针等内存管理功能
+#include <mutex>            // 用于线程同步
 
-namespace Kama_memoryPool
+namespace Kama_memoryPool    // 定义一个名为 Kama_memoryPool 的命名空间 在这个命名空间下的类和函数将属于这个内存池项目，避免与其他项目的命名冲突
 {
-#define MEMORY_POOL_NUM 64
-#define SLOT_BASE_SIZE 8
-#define MAX_SLOT_SIZE 512
+#define MEMORY_POOL_NUM 64   // 表示内存池中槽的数量
+#define SLOT_BASE_SIZE 8     // 内存池槽的基本大小（8字节）
+#define MAX_SLOT_SIZE 512    // 内存池槽的最大大小（512字节）
 
 
 /* 具体内存池的槽大小没法确定，因为每个内存池的槽大小不同(8的倍数)
    所以这个槽结构体的sizeof 不是实际的槽大小 */
-struct Slot 
+
+// Slot 结构体代表一个内存池的槽
+struct Slot                  //
 {
-    std::atomic<Slot*> next; // 原子指针
+    std::atomic<Slot*> next; // 原子指针   
+    
+    // 每个 Slot 对象都有一个 next 指针， 指向链表中的下一个空闲槽
+
+    // std::atomic 保证这个指针在多线程环境下的安全性。
+    // 在多线程编程中，原子指针保证了不会出现并发访问时的竞态条件
+
 };
+/* 我们为什么要这么做？
+
+   如果你要在多线程下操作 freeList_（头指针）链表，比如入队/出队，就需要修改：当前节点的 next 、全局 freeList_ 头指针
+   这些都是非原子的操作！ 在多个线程同时访问时，你根本没法保证 next 设置的是哪个版本，数据会错乱或崩溃。
+
+*/
+// ***原子性意味着操作不能被中断，保证在多线程环境下的正确性。****
 
 class MemoryPool
 {
@@ -30,32 +63,35 @@ public:
     void init(size_t);
 
     void* allocate();
-    void deallocate(void*);
+    void deallocate(void*);  
+    // void* 是 C++ 中的 "指向未知类型的指针"，它被称为**“空指针类型”**（void pointer）
+    // 可以将任何类型的指针（如 int*、float*、char* 等）转换为 void* 类型 反之亦然
+    // 但是，你不能直接对 void* 进行解引用，必须将其转换为一个已知类型的指针后才能访问其指向的数据。
 private:
-    void allocateNewBlock();
-    size_t padPointer(char* p, size_t align);
+    void allocateNewBlock();                  // 分配一个新的内存块     
+    size_t padPointer(char* p, size_t align); // 确保指针对齐 
 
     // 使用CAS操作进行无锁入队和出队
-    bool pushFreeList(Slot* slot);
-    Slot* popFreeList();
+    bool pushFreeList(Slot* slot);            // 向空闲槽列表中添加槽
+    Slot* popFreeList();                      // 向空闲槽列表中取出槽
 private:
-    int                 BlockSize_; // 内存块大小
-    int                 SlotSize_; // 槽大小
-    Slot*               firstBlock_; // 指向内存池管理的首个实际内存块
-    Slot*               curSlot_; // 指向当前未被使用过的槽
-    std::atomic<Slot*>  freeList_; // 指向空闲的槽(被使用过后又被释放的槽)
-    Slot*               lastSlot_; // 作为当前内存块中最后能够存放元素的位置标识(超过该位置需申请新的内存块)
+    int                 BlockSize_;         // 内存块大小
+    int                 SlotSize_;          // 槽大小
+    Slot*               firstBlock_;        // 指向内存池管理的首个实际内存块
+    Slot*               curSlot_;           // 指向当前未被使用过的槽
+    std::atomic<Slot*>  freeList_;          // 指向空闲的槽(被使用过后又被释放的槽)
+    Slot*               lastSlot_;          // 作为当前内存块中最后能够存放元素的位置标识(超过该位置需申请新的内存块)
     //std::mutex          mutexForFreeList_; // 保证freeList_在多线程中操作的原子性
-    std::mutex          mutexForBlock_; // 保证多线程情况下避免不必要的重复开辟内存导致的浪费行为
+    std::mutex          mutexForBlock_;      // 保证多线程情况下避免不必要的重复开辟内存导致的浪费行为
 };
 
 class HashBucket
 {
 public:
     static void initMemoryPool();
-    static MemoryPool& getMemoryPool(int index);
+    static MemoryPool& getMemoryPool(int index);          // 获取指定索引的内存池
 
-    static void* useMemory(size_t size)
+    static void* useMemory(size_t size)                   // 根据内存大小选择合适的内存池，分配内存
     {
         if (size <= 0)
             return nullptr;
@@ -64,6 +100,7 @@ public:
 
         // 相当于size / 8 向上取整（因为分配内存只能大不能小
         return getMemoryPool(((size + 7) / SLOT_BASE_SIZE) - 1).allocate();
+        // 如果直接size/SLOT_BASE_SIZE 当size是SLOT_BASE_SIZE整数倍的时候  会出现错误  破坏这个区间映射
     }
 
     static void freeMemory(void* ptr, size_t size)
@@ -94,9 +131,16 @@ T* newElement(Args&&... args)
     if ((p = reinterpret_cast<T*>(HashBucket::useMemory(sizeof(T)))) != nullptr)
         // 在分配的内存上构造对象
         new(p) T(std::forward<Args>(args)...);
+        // std::forward<Args>(args)... 通过完美转发将原始参数传递给构造函数。std::forward 保证参数的左值/右值特性被正确保留
 
     return p;
 }
+//  template<typename T, typename... Args> 这是 模板声明，表示 newElement 是一个模板函数。
+//  Args... 是一个可变模板参数包 表示这个函数可以接受多个类型的参数，并将它们传递给构造函数
+//  Args&&... args 是 完美转发（Perfect Forwarding）的技巧，使得你传递给 newElement 的参数能够保持其原有类型（包括左值和右值的区分）
+
+//  T 是一个模板类型参数，表示你将创建的对象类型 
+//  表示该函数返回一个指向 T 类型的指针
 
 template<typename T>
 void deleteElement(T* p)
--- v1/src/MemoryPool.cpp
+++ v1/src/MemoryPool.cpp
@@ -1,26 +1,30 @@
 #include "../include/MemoryPool.h"
-
+// #include "MemoryPool.h"：默认查找当前目录下的文件。
+// #include "../include/MemoryPool.h"：查找当前目录的父目录中的 include 文件夹下的 MemoryPool.h 文件。
+// ..：表示“当前目录的父目录”
 namespace Kama_memoryPool 
 {
 MemoryPool::MemoryPool(size_t BlockSize)
-    : BlockSize_ (BlockSize)
-    , SlotSize_ (0)
-    , firstBlock_ (nullptr)
-    , curSlot_ (nullptr)
-    , freeList_ (nullptr)
-    , lastSlot_ (nullptr)
+    : BlockSize_ (BlockSize)      // 每个内存块的大小
+    , SlotSize_ (0)               // 槽大小
+    , firstBlock_ (nullptr)       // 指向内存池管理的首个实际内存块
+    , curSlot_ (nullptr)          // 指向当前未被使用过的槽
+    , freeList_ (nullptr)         // 指向空闲的槽(被使用过后又被释放的槽)
+    , lastSlot_ (nullptr)         // 作为当前内存块中最后能够存放元素的位置标识(超过该位置需申请新的内存块)
 {}
 
 MemoryPool::~MemoryPool()
 {
     // 把连续的block删除
-    Slot* cur = firstBlock_;
+    Slot* cur = firstBlock_;  // firstBlock_ 指向你通过 operator new 分配的内存块头部
     while (cur)
     {
         Slot* next = cur->next;
         // 等同于 free(reinterpret_cast<void*>(firstBlock_));
         // 转化为 void 指针，因为 void 类型不需要调用析构函数，只释放空间
         operator delete(reinterpret_cast<void*>(cur));
+        // 顺着 next 遍历所有 block 把每一个 cur 转成 void*，然后 operator delete()；
+        // 这些块里不需要调用析构函数，因为你用的是原始内存块（不是 new 出来的对象数组）
         cur = next;
     }
 }
@@ -28,6 +32,7 @@ MemoryPool::~MemoryPool()
 void MemoryPool::init(size_t size)
 {
     assert(size > 0);
+    // assert 是一个“调试断言”，意思是“我假设这里必须成立，如果不成立，我宁愿程序立刻崩溃以便发现 bug”
     SlotSize_ = size;
     firstBlock_ = nullptr;
     curSlot_ = nullptr;
@@ -65,13 +70,14 @@ void MemoryPool::deallocate(void* ptr)
 
     Slot* slot = reinterpret_cast<Slot*>(ptr);
     pushFreeList(slot);
+    // 当一个槽被释放时，它的 next 指针会被设置为之前的 freeList 头节点，然后更新 freeList。
 }
 
 void MemoryPool::allocateNewBlock()
 {   
     //std::cout << "申请一块内存块，SlotSize: " << SlotSize_ << std::endl;
     // 头插法插入新的内存块
-    void* newBlock = operator new(BlockSize_);
+    void* newBlock = operator new(BlockSize_);           // void* 表示这块内存是通用的，不指定类型
     reinterpret_cast<Slot*>(newBlock)->next = firstBlock_;
     firstBlock_ = reinterpret_cast<Slot*>(newBlock);
 
@@ -83,6 +89,8 @@ void MemoryPool::allocateNewBlock()
     lastSlot_ = reinterpret_cast<Slot*>(reinterpret_cast<size_t>(newBlock) + BlockSize_ - SlotSize_ + 1);
 
     freeList_ = nullptr;
+
+    // reinterpret_cast 用于操作裸内存
 }
 
 // 让指针对齐到槽大小的倍数位置
@@ -98,9 +106,9 @@ bool MemoryPool::pushFreeList(Slot* slot)
     while (true)
     {
         // 获取当前头节点
-        Slot* oldHead = freeList_.load(std::memory_order_relaxed);
+        Slot* oldHead = freeList_.load(std::memory_order_relaxed); // .load()：读取原子变量的当前值
         // 将新节点的 next 指向当前头节点
-        slot->next.store(oldHead, std::memory_order_relaxed);
+        slot->next.store(oldHead, std::memory_order_relaxed);      // .store(val)：写入值
 
         // 尝试将新节点设置为头节点
         if (freeList_.compare_exchange_weak(oldHead, slot,
@@ -108,6 +116,18 @@ bool MemoryPool::pushFreeList(Slot* slot)
         {
             return true;
         }
+        // 多线程访问 + 至少有一个写操作，就叫数据竞争（data race），就需要用同步手段来保障“看到的数据是对的”
+
+
+        //.compare_exchange_weak(old, new)： 如果当前值 == old → 改成 new → 返回 true； 否则 → 返回 false；
+        // 经典的 CAS（Compare-And-Swap）操作！
+
+        // memory_order 控制并发下读写的执行顺序与安全性       一种编译器/CPU 指令的“提示”或“约束”
+        // memory_order_relaxed 最宽松，无序
+        // memory_order_release 写屏障，之前的写不能被延后
+
+        // release + acquire 配对就像设置了“读写屏障”，强制顺序不会错！
+
         // 失败：说明另一个线程可能已经修改了 freeList_
         // CAS 失败则重试
     }
@@ -118,17 +138,18 @@ Slot* MemoryPool::popFreeList()
 {
     while (true)
     {
-        Slot* oldHead = freeList_.load(std::memory_order_acquire);
+        Slot* oldHead = freeList_.load(std::memory_order_acquire); 
+        // acquire 确保“后面所有读操作”必须在它之后执行；
         if (oldHead == nullptr)
             return nullptr; // 队列为空
 
         // 在访问 newHead 之前再次验证 oldHead 的有效性
         Slot* newHead = nullptr;
-        try
+        try                              // try 块用来捕获可能会抛出的异常
         {
             newHead = oldHead->next.load(std::memory_order_relaxed);
         }
-        catch(...)
+        catch(...)                       // catch 块用来处理这些异常 ... 捕获所有类型的异常
         {
             // 如果返回失败，则continue重新尝试申请内存
             continue;
@@ -151,7 +172,7 @@ void HashBucket::initMemoryPool()
 {
     for (int i = 0; i < MEMORY_POOL_NUM; i++)
     {
-        getMemoryPool(i).init((i + 1) * SLOT_BASE_SIZE);
+        getMemoryPool(i).init((i + 1) * SLOT_BASE_SIZE); // “引用+函数链式调用”
     }
 }   
 
@@ -159,7 +180,23 @@ void HashBucket::initMemoryPool()
 MemoryPool& HashBucket::getMemoryPool(int index)
 {
     static MemoryPool memoryPool[MEMORY_POOL_NUM];
+    // static 保证内存池的唯一性与持续性  创建并初始化 memoryPool[] 后续所有调用：都使用这个已经初始化好的那一份
+    // 静态局部变量，只初始化一次，在整个程序运行期间全局保留 
+    // 声明了一个MemoryPool 对象的数组，数组长度是常量 MEMORY_POOL_NUM
+
+
+    // 整个 memoryPool[] 这块静态数组，只有第一次调用 getMemoryPool() 函数时才创建，整个程序运行期间只构造一次
+    // 不是 一上来程序启动就分配
     return memoryPool[index];
+
+    /*                    // 按需构造第 N 个池子
+    static std::unique_ptr<MemoryPool> memoryPools[64];
+
+    if (!memoryPools[index]) {
+        memoryPools[index] = std::make_unique<MemoryPool>();
+    }
+    return *memoryPools[index];
+    */ 
 }
 
 } // namespace memoryPool
